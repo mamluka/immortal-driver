0b1b3e44629c87a2c7c82c96b618d154605d56a9
gilly <gilly@delver.com>
2012-06-21 01:35:20 +0300

    modifications to the session_manager and the request_handler

--- ImmortalDriver/server_request_handler.js
+++ ImmortalDriver/server_request_handler.js
@@ -1,41 +1,80 @@
 var immortalServer = immortalServer || {};
 
 immortalServer.serverRequestHandler = function(request, response) {
+	casper.echo('');
 	casper.echo('incoming request => ' + JSON.stringify(request), 'DEBUG');
-	
+
 	// parse the request url
 	var requestedUrl = request.url;
+	var method = request.method;
 	var splitUrl = requestedUrl.substring(1).split('/');
-	
-	if (splitUrl.length == 1) { // /session, /sessions, /status
-		response.statusCode = 303;
-		immortalServer.respond(response);
-	}
-	else if (splitUrl.length == 2) { // /session/:sessionId (GET/POST)
-		response.statusCode = 200;
-		immortalServer.respond(response, {javascriptEnabled:true});
-	}
-	else {
-		if (splitUrl[2] === 'element' || splitUrl === 'elements') {
-		
+
+	if (splitUrl.length === 1) { // /session, /sessions, /status
+		if (splitUrl[0] === 'session' && method === 'POST') { // request to start a new session
+			casper.log('got session request');
+			casper.log(typeof immortalServer.sessionManager);
+			immortalServer.sessionManager.createNewSession();
+			response.statusCode = 303;
+			immortalServer.respond(response);
+		}
+		else if (splitUrl[0] === 'sessions') {
+
+		}
+		else if (splitUrl[0] === 'status') {
+
 		}
 	}
-	
+	else if (splitUrl.length === 2) { // /session/:sessionId (GET/DELETE)
+		if (splitUrl[0] === 'session' && typeof (splitUrl[1]) === 'number') {
+			if (method === 'GET') {
+				response.statusCode = 200;
+				immortalServer.respond(response, immortalServer.sessionManager.defaultCapabilities);
+			}
+			else if (method === 'DELETE') {
+				response.statusCode = 200;
+				immortalServer.respond(response, immortalServer.sessionManager.defaultCapabilities);
+			}
+		}
+	}
+	else if (splitUrl[0] === 'session' && typeof (splitUrl[1]) === 'number') {
+
+		immortalServer.sessionManager.setSession(splitUrl[1]);
+
+		if (splitUrl[2] === 'url' && method === 'GET') { // get page title
+			var pageTitle;
+			pageTitle = immortalServer.sessionManager.currentSession.evaluate(function() {
+				return document.title;
+			});
+			responseStatusCode = 200;
+			immortalServer.respond(response, pageTitle);
+		}
+		else if (splitUrl[2] === 'url' && method === 'POST') { // navigate to url
+			immortalServer.sessionManager.currentSession.open(request.post.url, function() {
+				response.StatusCode = 200;
+				immortalServer.respond(response);
+			});
+		}
+		else if (splitUrl[2] === 'element' || splitUrl === 'elements') {
+
+		}
+	}
+
 	// TODO: catch unsupported requests here
 };
 
 immortalServer.respond = function(response, value, status, sessionId) {
 	response.setHeader("Cache", "no-cache");
 	response.setHeader("Content-Type", "application/json;charset=utf-8");
-	
+
 	var returnObject = {
 		sessionId: sessionId || 1, // TODO: retrieve this from our session manager
 		status: status || 0, // default status is 'success'
 		value: value || {} // default is an empty json object
 	};
-	
+
 	response.write(JSON.stringify(returnObject));
 	response.close();
-	
+
+	casper.echo('');
 	casper.echo('response sent => ' + JSON.stringify(returnObject), 'DEBUG');
 };
--- ImmortalDriver/session_manager.js
+++ ImmortalDriver/session_manager.js
@@ -1,13 +1,49 @@
 var immortalServer = immortalServer || {};
 
-immortalServer.sessionCounter = 0;
-immortalServer.activeSessions = immortalServer.activeSessions || [];
+// Singleton - There should only be one sessionManager instance
+immortalServer.sessionManager = (function() {
 
-immortalServer.sessionManager = function() {
-	
-	this.sessionId = immortalServer.sessionCounter++;
-	
-	this.Capabilities = {
+	var _sessionCounter = 0;
+	var _currentSession = 0;
+	var _sessions = [];
+
+	var _createNewSession = function() {
+		casper.log('creating new session');
+		var newSession = require('webpage').create();
+		newSession.sessionId = _sessionCounter;
+		_sessions.push(newSession);
+		_currentSession = _sessionCounter;
+		_sessionCounter++;
+		casper.log('done creating session');
+	};
+
+	var _deleteSession = function(sessionId) {
+		if (typeof (sessionId) !== 'number')
+		// TODO: throw some kind of error!
+			return;
+
+		if (sessionId > _sessions.length - 1)
+		// TODO: throw some kind of error! This shouldn't happen
+			return;
+
+		_sessions[sessionId] = null;
+		if (_currentSession === sessionId)
+			_currentSession = null;
+	};
+
+	var _setSession = function(sessionId) {
+		if (typeof (sessionId) !== 'number')
+		// TODO: throw some kind of error instead;
+			return;
+
+		if (sessionId > _sessions.length - 1 || _sessions[sessionId] === null)
+		// TODO: throw an error
+			return;
+
+		_currentSession = sessionId;
+	};
+
+	var _defaultCapabilities = {
 		browserName: 'phantomjs',
 		version: phantom.version.major + '.' + phantom.version.minor + '.' + phantom.version.patch,
 		platform: phantom.defaultPageSettings.userAgent,
@@ -25,5 +61,13 @@ immortalServer.sessionManager = function() {
 		nativeEvents: true,
 		proxy: { proxyType: 'direct' }
 	};
-	
-};
+
+	return {
+		createNewSession: _createNewSession,
+		deleteSession: _deleteSession,
+		setSession: _setSession,
+		currentSession: _sessions[_currentSession],
+		defaultCapabilities: _defaultCapabilities
+	};
+
+})();
