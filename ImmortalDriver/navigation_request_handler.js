c945d144d0ce3edeefb7564752418b2421a37cd6
gillyb <gillyb@gmail.com>
2012-07-04 22:35:05 +0300

    deleted commented code

--- ImmortalDriver/navigation_request_handler.js
+++ ImmortalDriver/navigation_request_handler.js
@@ -64,52 +64,6 @@ immortalServer.requestHandlers.push({
 				immortalServer.respond(response);
 			}
 		}
-	}/*,
-
-	handleRequest: function(request, response) {
-		var splitUrl = request.splitUrl;
-		var requestedSessionId = parseInt(splitUrl[1]);
-		// TODO: check that requestedSessionId is really a number - if not then throw some error!
-		// TODO: create something generic to set the sessionId
-		immortalServer.sessionManager.setSession(parseInt(splitUrl[1]));
-
-		if (splitUrl[2] === 'url' && request.method == 'GET') { // get page title
-			var pageTitle = immortalServer.sessionManager.getCurrentSession().evaluate(function() {
-				return document.location.href;
-			});
-			responseStatusCode = 200;
-			immortalServer.respond(response, pageTitle);
-		}
-		else if (splitUrl[2] == 'url' && request.method == 'POST') { // navigate to url
-			casper.log('request to navigate to specific url', 'INFO');
-			immortalServer.sessionManager.getCurrentSession().open(request.POST.url, function() {
-				response.StatusCode = 200;
-				immortalServer.respond(response);
-			});
-		}
-		else if (splitUrl[2] == 'back' && request.method == 'POST') {
-			casper.echo(JSON.stringify(immortalServer.sessionManager.getCurrentSession()), 'DEBUG');
-			var a = immortalServer.sessionManager.getCurrentSession().evaluate(function() {
-				document.defaultView.history.go(-1);
-			});
-			casper.echo(a, 'DEBUG');
-			responseStatusCode = 200;
-			immortalServer.respond(response);
-		}
-		else if (splitUrl[2] == 'forward' && request.method == 'POST') {
-			immortalServer.sessionManager.getCurrentSession().evaluate(function() {
-				document.defaultView.history.go(1);
-			});
-			responseStatusCode = 200;
-			immortalServer.respond(response);
-		}
-		else if (splitUrl[2] == 'refresh' && request.method == 'POST') {
-			immortalServer.sessionManager.getCurrentSession().evaluate(function() {
-				document.defaultView.history.go();
-			});
-			responseStatusCode = 200;
-			immortalServer.respond(response);
-		}
-	}*/
+	}
 
 });
--- ImmortalDriver/session_request_handler.js
+++ ImmortalDriver/session_request_handler.js
@@ -33,29 +33,6 @@ immortalServer.requestHandlers.push({
 		sessions: {
 			GET: function(request, response) { }
 		}
-	}/*,
-
-	handleRequest: function(request, response) {
-		var splitUrl = request.splitUrl;
-
-		if (splitUrl.length == 1 && splitUrl[0] == 'session' && request.method == 'POST') {
-			immortalServer.sessionManager.createNewSession();
-			response.statusCode = 303;
-			immortalServer.respond(response);
-		}
-		else if (splitUrl.length == 2 && splitUrl[0] == 'session' && typeof (splitUrl[1]) == 'number') {
-			if (request.method == 'GET') {
-				// TODO: return actual requested session data!
-				response.statusCode = 200;
-				immortalServer.respond(response, immortalServer.sessionManager.defaultCapabilities);
-			}
-			else if (request.method == 'DELETE') {
-				// TODO: delete the requested session!
-				response.statusCode = 200;
-				immortalServer.respond(response, immortalServer.sessionManager.defaultCapabilities);
-			}
-		}
-
-	}*/
+	}
 
 });
