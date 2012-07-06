4e7746f571d1adc733bf2b46ef26f41447627388
gillyb <gillyb@gmail.com>
2012-07-06 15:52:32 +0300

    beginning of the FindElement command implementation in javascript

--- ImmortalDriver/request_handlers/element_request_handler.js
+++ ImmortalDriver/request_handlers/element_request_handler.js
@@ -16,8 +16,17 @@ immortalDriver.requestHandlers.push({
 			POST: function(request, response) {
 				immortalDriver.sessionManager.setSession(parseInt(request.splitUrl[1]));
 				console.log('incoming element request');
-				// request.POST.using - this is the strategy (css selected, class name, id, name, link text, partial link text, tag name, xpath)
+				var p = request.POST;
+				// request.POST.using - this is the strategy (css selector, class name, id, name, link text, partial link text, tag name, xpath)
 				// request.POST.value - this is the search value
+				var nodeList = immortalDriver.sessionManager.getCurrentSession().evaluate(function() {
+					return document.querySelector('body div#main h2');
+					// TODO: if i change the string inside querySelector to be p.value, this doesn't work.
+					// TODO: why can't i use the 'request' variable inside the 'evaluate' method scope ???
+				});
+				console.log('nodeList = ' + JSON.stringify(nodeList));
+				response.statusCode = 200;
+				immortalDriver.respond(response, pageSource);
 			}
 		}
 	}
