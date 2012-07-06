652d6c0f65b40386e5c3f86dbf5e436564640330
gillyb <gillyb@gmail.com>
2012-07-06 15:18:25 +0300

    changed the design of the session manager

--- ImmortalDriver/session_manager.js
+++ ImmortalDriver/session_manager.js
@@ -1,52 +1,53 @@
 var immortalServer = immortalServer || {};
 
-// TODO: change the method names here, to match our standard!
-//
-// Singleton - There should only be one sessionManager instance
-immortalServer.sessionManager = (function() {
+immortalServer.sessionManager = {
 
-	var _sessionCounter = 0;
-	var _currentSession = 0;
-	var _sessions = [];
+	sessionCounter: 0,
+	currentSession: 0,
+	sessions: [],
 
-	var _createNewSession = function() {
+	createNewSession: function() {
 		casper.log('creating new session');
 		var newSession = require('webpage').create();
-		newSession.sessionId = _sessionCounter;
-		_sessions.push(newSession);
-		_currentSession = _sessionCounter;
-		_sessionCounter++;
-	};
+		newSession.sessionId = this.sessionCounter;
+		this.sessions.push(newSession);
+		this.currentSession = this.sessionCounter;
+		this.sessionCounter++;
+	},
 
-	var _deleteSession = function(sessionId) {
-		if (typeof (sessionId) !== 'number')
+	deleteSession: function(sessionId) {
+		if (typeof (sessionId) != 'number')
 		// TODO: throw some kind of error!
 			return;
 
-		if (sessionId > _sessions.length - 1)
+		if (sessionId > this.sessions.length - 1)
 		// TODO: throw some kind of error! This shouldn't happen
 			return;
 
-		_sessions[sessionId].release();
+		this.sessions[sessionId].release();
 		casper.echo('session ' + sessionId + ' was released');
-		_sessions[sessionId] = null;
-		if (_currentSession === sessionId)
-			_currentSession = null;
-	};
+		this.sessions[sessionId] = null;
+		if (this.currentSession === sessionId)
+			this.currentSession = null;
+	},
 
-	var _setSession = function(sessionId) {
-		if (typeof (sessionId) !== 'number')
+	setSession: function(sessionId) {
+		if (typeof (sessionId) != 'number')
 		// TODO: throw some kind of error instead;
 			return;
 
-		if (sessionId > _sessions.length - 1 || _sessions[sessionId] === null)
+		if (sessionId > this.sessions.length - 1 || this.sessions[sessionId] === null)
 		// TODO: throw an error
 			return;
 
-		_currentSession = sessionId;
-	};
+		this.currentSession = sessionId;
+	},
 
-	var _defaultCapabilities = {
+	getCurrentSession: function() {
+		return this.sessions[this.currentSession];
+	},
+
+	defaultCapabilities: {
 		browserName: 'phantomjs',
 		version: phantom.version.major + '.' + phantom.version.minor + '.' + phantom.version.patch,
 		platform: phantom.defaultPageSettings.userAgent,
@@ -63,14 +64,6 @@ immortalServer.sessionManager = (function() {
 		acceptSslCerts: false,
 		nativeEvents: true,
 		proxy: { proxyType: 'direct' }
-	};
+	}
 
-	return {
-		createNewSession: _createNewSession,
-		deleteSession: _deleteSession,
-		setSession: _setSession,
-		getCurrentSession: function() { return _sessions[_currentSession]; },
-		defaultCapabilities: _defaultCapabilities
-	};
-
-})();
+};
