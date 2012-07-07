760b57bd756faf656aaf18c66eb9426ab0ca6dfd
gillyb <gillyb@gmail.com>
2012-07-07 14:58:30 +0300

    got the parameter into the evaluate method in the 'elemen' command

--- ImmortalDriver/request_handlers/element_request_handler.js
+++ ImmortalDriver/request_handlers/element_request_handler.js
@@ -15,15 +15,11 @@ immortalDriver.requestHandlers.push({
 		element: {
 			POST: function(request, response) {
 				immortalDriver.sessionManager.setSession(parseInt(request.splitUrl[1]));
-				console.log('incoming element request');
-				var p = request.POST;
 				// request.POST.using - this is the strategy (css selector, class name, id, name, link text, partial link text, tag name, xpath)
 				// request.POST.value - this is the search value
-				var nodeList = immortalDriver.sessionManager.getCurrentSession().evaluate(function() {
-					return document.querySelector('body div#main h2');
-					// TODO: if i change the string inside querySelector to be p.value, this doesn't work.
-					// TODO: why can't i use the 'request' variable inside the 'evaluate' method scope ???
-				});
+				var nodeList = immortalDriver.sessionManager.getCurrentSession().evaluate(function(cssSelector) {
+					return document.querySelector(cssSelector);
+				}, request.POST.value);
 				console.log('nodeList = ' + JSON.stringify(nodeList));
 				response.statusCode = 200;
 				immortalDriver.respond(response, pageSource);
