fa6e771b4f65d2063ec13266d0b1f605bba7b187
gillyb <gillyb@gmail.com>
2012-07-07 15:26:54 +0300

    implemented more strategies in the 'element' command implementation

--- ImmortalDriver/request_handlers/element_request_handler.js
+++ ImmortalDriver/request_handlers/element_request_handler.js
@@ -4,7 +4,7 @@ immortalDriver.requestHandlers.push({
 	name: 'element_request_handler',
 
 	canHandleRequest: function(request) {
-		return request.splitUrl.length == 3 && request.splitUrl[2] == 'element';
+		return request.splitUrl.length == 3 && (request.splitUrl[2] == 'element' || request.splitUrl[2] == 'elements');
 	},
 
 	getMethodName: function(request) {
@@ -17,9 +17,26 @@ immortalDriver.requestHandlers.push({
 				immortalDriver.sessionManager.setSession(parseInt(request.splitUrl[1]));
 				// request.POST.using - this is the strategy (css selector, class name, id, name, link text, partial link text, tag name, xpath)
 				// request.POST.value - this is the search value
-				var nodeList = immortalDriver.sessionManager.getCurrentSession().evaluate(function(cssSelector) {
-					return document.querySelector(cssSelector);
-				}, request.POST.value);
+				var nodeList = immortalDriver.sessionManager.getCurrentSession().evaluate(function(selectorStrategy, selectorValue) {
+					switch (selectorStrategy) {
+						case 'css selector':
+							return document.querySelector(selectorValue);
+						case 'class name':
+							return document.getElementsByClassName(selectorValue);
+						case 'id':
+							return document.getElementById(selectorValue);
+						case 'name':
+							return document.getElementsByName(selectorValue);
+						case 'tag name':
+							return document.getElementsByTagName(selectorValue);
+						//case 'link text':
+						//case 'partial link text':
+						//case 'xpath':
+						default:
+							//TODO: report appropriate error
+							return false;
+					}
+				}, request.POST.using, request.POST.value);
 				console.log('nodeList = ' + JSON.stringify(nodeList));
 				response.statusCode = 200;
 				immortalDriver.respond(response, pageSource);
